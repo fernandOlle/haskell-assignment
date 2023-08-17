800a8ca887486289cec8388f4a43e9ba8af7d073
Fernando Ollé <55040050+fernandOlle@users.noreply.github.com>
2023-08-17 01:02:30 -0300

    Update assign.hs

--- assign.hs
+++ assign.hs
@@ -26,24 +26,20 @@ data C = While B C
     | DAtrrib E E E E -- Dupla atribuição: recebe duas variáveis "e1" e "e2" e duas expressões "e3" e "e4". Faz e1:=e3 e e2:=e4.
    deriving(Eq,Show)                
 
-
 -----------------------------------------------------
 -----
 ----- As próximas funções, servem para manipular a memória (sigma)
 -----
 ------------------------------------------------
 
-
 --- A próxima linha de código diz que o tipo memória é equivalente a uma lista de tuplas, onde o
 --- primeiro elemento da tupla é uma String (nome da variável) e o segundo um Inteiro
 --- (conteúdo da variável):
-
 
 type Memoria = [(String,Int)]
 
 exSigma :: Memoria
 exSigma = [ ("x", 10), ("temp",0), ("y",0)]
-
 
 --- A função procuraVar recebe uma memória, o nome de uma variável e retorna o conteúdo
 --- dessa variável na memória. Exemplo:
@@ -51,13 +47,11 @@ exSigma = [ ("x", 10), ("temp",0), ("y",0)]
 --- *Main> procuraVar exSigma "x"
 --- 10
 
-
 procuraVar :: Memoria -> String -> Int
 procuraVar [] s = error ("Variavel " ++ s ++ " nao definida no estado")
 procuraVar ((s,i):xs) v
   | s == v     = i
   | otherwise  = procuraVar xs v
-
 
 --- A função mudaVar, recebe uma memória, o nome de uma variável e um novo conteúdo para essa
 --- variável e devolve uma nova memória modificada com a varíável contendo o novo conteúdo. A
@@ -74,7 +68,6 @@ mudaVar [] v n = error ("Variavel " ++ v ++ " nao definida no estado")
 mudaVar ((s,i):xs) v n
   | s == v     = ((s,n):xs)
   | otherwise  = (s,i): mudaVar xs v n
-
 
 -------------------------------------
 ---
@@ -116,7 +109,6 @@ bbigStep (Leq e1 e2,s) = ebigStep (e1,s) <= ebigStep (e2,s)
 --bbigStep (Igual e1 e2) = -- recebe duas expressões aritméticas e devolve um valor booleano dizendo se são iguais
 bbigStep (Igual e1 e2,s) = ebigStep (e1,s) == ebigStep (e2,s)
 
-
 cbigStep :: (C,Memoria) -> (C,Memoria)
 cbigStep (Skip,s) = (Skip,s)
 
@@ -138,10 +130,6 @@ cbigStep (Atrib (Var x) e,s) =
 cbigStep (While b c, s)
    | bbigStep (b,s)             = cbigStep (Seq c (While b c),s)
    | otherwise                  = cbigStep (Skip,s)
-
--- cbigStep (While b c,s)
---  | cbigStep (While TRUE c,s) = cbigStep (Seq (c,s) (cbigStep While b (cbigStep (c,s))),s)
---  | cbigStep (While FALSE c,s) = cbigStep (c,s)
 
 --cbigStep (DoWhile c b,s)
 cbigStep (DoWhile c b,s)
@@ -171,7 +159,6 @@ cbigStep (DAtrrib e1 e2 e3 e4,s)
 exSigma2 :: Memoria
 exSigma2 = [("x",3), ("y",0), ("z",0)]
 
-
 ---
 --- O progExp1 é um programa que usa apenas a semântica das expressões aritméticas. Esse
 --- programa já é possível rodar com a implementação inicial  fornecida:
@@ -189,10 +176,8 @@ progExp1 = Soma (Num 3) (Soma (Var "x") (Var "y"))
 --- Para rodar os próximos programas é necessário primeiro implementar as regras da semântica
 ---
 
-
 ---
 --- Exemplos de expressões booleanas:
-
 
 teste1 :: B
 teste1 = (Leq (Soma (Num 3) (Num 3))  (Mult (Num 2) (Num 3)))
@@ -230,25 +215,3 @@ duplaAtribEx = (DAtrrib (Var "x") (Var "y") (Num 1) (Num 2))
 --- * Do While
 doWhileEx :: C
 doWhileEx = (DoWhile (Atrib (Var "x") (Soma (Var "x") (Num 1))) (Leq (Var "x") (Num 8)))
-
-
--- main :: IO ()
--- main = do
---    cbigStep (loopEx, exSigma3)
---    cbigStep (duplaAtrib, exSigma3)
---    cbigStep (doWhileEx, exSigma3)
-
-    -- let initialState1 = exSigma3
-    -- let (finalCommand1, finalMemory1) = cbigStep (loopEx, initialState1)
-    -- putStrLn "Loop Example - Final Memory:"
-    -- print finalMemory1
-
-    -- let initialState2 = exSigma2
-    -- let (finalCommand2, finalMemory2) = cbigStep (duplaAtrib, initialState2)
-    -- putStrLn "Dupla Atribuição Example - Final Memory:"
-    -- print finalMemory2
-
-    -- let initialState3 = exSigma3
-    -- let (finalCommand3, finalMemory3) = cbigStep (doWhileEx, initialState3)
-    -- putStrLn "Do While Example - Final Memory:"
-    -- print finalMemory3
